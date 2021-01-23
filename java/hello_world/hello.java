8989abe66bf78f7b5f5ada58c1b4b211de0bcc51
kootsoop <peter@kootsoop.com>
2021-01-23 15:25:12 -0500

    Added hello.java

---
+++ java/hello_world/hello.java
@@ -0,0 +1,20 @@
+/*
+ * This is the starter code for the Hello World Java assignment
+ * in CPSC 2232 for Fairfield University.
+ * 
+ * (c) Peter J. Kootsookos
+ * 
+ */
+
+import java.util.*;
+
+class Main
+{  
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+        System.out.println("0");
+    }
+}  
+
